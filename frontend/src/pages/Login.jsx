b3cd593c1017e40dfe033b55aa233f985d06845f
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-04-25 15:22:08 +0530

    sign-up pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+/*import { useState } from "react";
 
 function Login() {
   const [isSignup, setIsSignup] = useState(false);
@@ -18,6 +18,86 @@ function Login() {
       </form>
 
       <p onClick={() => setIsSignup(!isSignup)} style={{cursor:"pointer"}}>
+        {isSignup
+          ? "Already have an account? Login"
+          : "Don't have an account? Sign Up"}
+      </p>
+    </div>
+  );
+}
+
+export default Login;  */
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [isSignup, setIsSignup] = useState(false);
+  const [form, setForm] = useState({});
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const url = isSignup
+      ? "http://localhost:5000/api/auth/signup"
+      : "http://localhost:5000/api/auth/login";
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    });
+
+    const data = await res.json();
+
+    if (!isSignup) {
+      localStorage.setItem("token", data.token);
+
+      if (data.role === "admin") {
+        navigate("/admin");
+      } else {
+        navigate("/");
+      }
+    } else {
+      alert("Signup success, now login");
+      setIsSignup(false);
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <h2>{isSignup ? "Sign Up" : "Login"}</h2>
+
+      <form onSubmit={handleSubmit}>
+        {isSignup && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+          />
+        )}
+
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+
+        <button type="submit">
+          {isSignup ? "Sign Up" : "Login"}
+        </button>
+      </form>
+
+      <p onClick={() => setIsSignup(!isSignup)} style={{ cursor: "pointer" }}>
         {isSignup
           ? "Already have an account? Login"
           : "Don't have an account? Sign Up"}
